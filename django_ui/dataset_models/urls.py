fd19ff07ff62f3bf930526b5abcb0999f0b8dffd
taha <taha.maj4@gmail.com>
2025-08-04 13:31:44 +0330

    add files

--- django_ui/dataset_models/urls.py
+++ django_ui/dataset_models/urls.py
@@ -13,7 +13,11 @@ urlpatterns = [
     path('datasets/<uuid:dataset_id>/', views.dataset_detail, name='dataset_detail'),
     path('models/<uuid:model_id>/', views.model_detail, name='model_detail'),
     
+    # Dataset creation
+    path('create-dataset/', views.dataset_creation_wizard, name='dataset_creation_wizard'),
+    
     # API endpoints
     path('api/suggestions/', views.api_search_suggestions, name='api_suggestions'),
     path('api/generate-analysis/', views.api_generate_dataset_analysis, name='api_generate_analysis'),
+    path('api/dataset-suggestions/', views.api_generate_dataset_suggestions, name='api_dataset_suggestions'),
 ]
